b5ac0591112541f33136d74263551821b319b150
Timwise2020 <peterstimothy155@gmail.com>
2025-09-20 13:40:05 +0100

    project 8 edit

---
+++ index.js
@@ -0,0 +1,10 @@
+const form = document . getElementById ("form")
+const  fullname= document . getElementById ("fullname")
+const email = document . getElementById ("email")
+const submit = document . getElementById ("submit")
+
+form.addEventListener ("submit", function (e){
+document . getElementById("responceMsg").innerText = "sending message...";});
+// }
+//     e . prevent default () ;
+// )
